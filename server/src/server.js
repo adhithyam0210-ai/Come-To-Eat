557492b0126c5f5f335c445737bda6ff7ed843a3
adhithyam0210-ai <adhithyam0210@gmail.com>
2026-09-17 16:32:59 +0530

    Key added

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,9 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const dotenv = require('dotenv');
 const { initSchema } = require('./database');
 const apiRoutes = require('./routes/api');
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
